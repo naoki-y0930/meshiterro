f070ec3d6430297d7de1e3e9a169bd8024622209
山本直樹 <yamamotonaoki@ny-mbp.local>
2019-08-04 21:17:58 +0900

    [Finish] 5-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '3eb97beae37660abf010132f34d190d250a80a409c0806036f5ae62c1ca458469117a2355a6ecf926c735220882366f56e961e8cad217427afc0fdefc467fc96'
